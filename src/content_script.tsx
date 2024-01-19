3c7cdd8d7b3e46df0c932f93aa7db30449da91c2
xxxasdfghjk <xxxasdfhjk999@gmail.com>
2024-01-20 02:45:38 +0900

    ✨ 機能実装

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,9 +1,56 @@
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-  if (msg.color) {
-    console.log("Receive color = " + msg.color);
-    document.body.style.backgroundColor = msg.color;
-    sendResponse("Change color to " + msg.color);
-  } else {
-    sendResponse("Color message is none.");
-  }
+    if (msg.color) {
+        console.log("Receive color = " + msg.color);
+        document.body.style.backgroundColor = msg.color;
+        sendResponse("Change color to " + msg.color);
+    } else {
+        sendResponse("Color message is none.");
+    }
 });
+
+const getInfoFromCommentDOM = (record: HTMLDivElement) => {
+    const timestamp = record.querySelector("#timestamp")?.textContent;
+    const message = record.querySelector("#message")?.textContent;
+    const authorName = record.querySelector("#author-name")?.textContent;
+
+    return { timestamp, message, authorName };
+};
+
+const replaceDomText = (record: HTMLDivElement) => {
+    const text = record.querySelector("#message")?.textContent;
+    if (text)
+        if (record.querySelector("#message")?.textContent)
+            record.querySelector("#message")!.textContent = text.replaceAll("草", "lol");
+};
+
+const getCommentsDom = () => {
+    return (
+        document.querySelector("#chat > iframe") as HTMLIFrameElement | null
+    )?.contentWindow?.document.querySelector("yt-live-chat-item-list-renderer #items");
+};
+
+const onLoad = () => {
+    const commentsDOM = getCommentsDom();
+    const observer = new MutationObserver((records) => {
+        records.forEach((record) => {
+            Array.from(record.addedNodes).forEach((comment) => {
+                replaceDomText(comment as HTMLDivElement);
+            });
+        });
+    });
+    if (commentsDOM)
+        observer.observe(commentsDOM, {
+            childList: true,
+        });
+};
+
+const main = () => {
+    const jsInitCheckTimer = setInterval(() => {
+        const target = getCommentsDom();
+        if (target?.childNodes.length ?? 0 != 0) {
+            clearInterval(jsInitCheckTimer);
+            onLoad();
+        }
+    }, 1000);
+};
+window.addEventListener("load", main, false);
